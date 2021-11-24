c76ab93a8027169dec6578e6bbe6a5c974da257b
Joe Burnard <spongybacon@hotmail.co.uk>
2021-11-24 23:07:55 +0000

    add account_link to resources.rs

--- src/resources.rs
+++ src/resources.rs
@@ -164,6 +164,8 @@ pub use self::tax_rate::*;
 #[cfg(feature = "connect")]
 mod account;
 #[cfg(feature = "connect")]
+mod account_link;
+#[cfg(feature = "connect")]
 mod application;
 #[cfg(feature = "connect")]
 mod application_fee;
@@ -183,6 +185,8 @@ mod transfer;
 mod transfer_reversal;
 #[cfg(feature = "connect")]
 pub use self::account::*;
+#[cfg(feature = "connect")]
+pub use self::account_link::*;
 #[cfg(feature = "connect")]
 pub use self::application::*;
 #[cfg(feature = "connect")]
